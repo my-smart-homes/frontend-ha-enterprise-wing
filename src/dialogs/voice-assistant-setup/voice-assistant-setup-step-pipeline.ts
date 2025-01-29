783deb72b6d809b83dc0c045c647858eef68eb7c
Naimur <naimurhasanrwd@gmail.com>
2025-01-29 15:56:18 +0600

    f-patch
    fix: state annotation missing
    
    fix: translation username
    
    tip closed
    
    replace msh with ha
    
    list change
    
    update search
    
    fix: add integration count missing
    
    replace: ts files home assistant -> msh
    
    bump version
    
    fix: list display bug fix

--- src/dialogs/voice-assistant-setup/voice-assistant-setup-step-pipeline.ts
+++ src/dialogs/voice-assistant-setup/voice-assistant-setup-step-pipeline.ts
@@ -106,7 +106,7 @@ export class HaVoiceAssistantSetupStepPipeline extends LitElement {
               </div>`
             : nothing}
         </div>
-        <h2>Home Assistant Cloud</h2>
+        <h2>MSH Cloud</h2>
         <p>
           ${this.hass.localize(
             "ui.panel.config.voice_assistants.satellite_wizard.pipeline.cloud.description"
@@ -258,7 +258,7 @@ export class HaVoiceAssistantSetupStepPipeline extends LitElement {
         )
       ).providers.find((provider) => provider.engine_id === cloudSttEntityId);
 
-      let pipelineName = "Home Assistant Cloud";
+      let pipelineName = "MSH Cloud";
       let i = 1;
       while (
         pipelines.pipelines.find(
@@ -266,7 +266,7 @@ export class HaVoiceAssistantSetupStepPipeline extends LitElement {
           (pipeline) => pipeline.name === pipelineName
         )
       ) {
-        pipelineName = `Home Assistant Cloud ${i}`;
+        pipelineName = `MSH Cloud ${i}`;
         i++;
       }
 
--- src/onboarding/onboarding-create-user.ts
+++ src/onboarding/onboarding-create-user.ts
@@ -15,8 +15,9 @@ import { onboardUserStep } from "../data/onboarding";
 import type { ValueChangedEvent } from "../types";
 import { onBoardingStyles } from "./styles";
 import { debounce } from "../common/util/debounce";
+import "./onboarding-loading";
 
-const CHECK_USERNAME_REGEX = /\s|[A-Z]/;
+// const CHECK_USERNAME_REGEX = /\s|[A-Z]/;
 
 const CREATE_USER_SCHEMA: HaFormSchema[] = [
   {
@@ -27,7 +28,7 @@ const CREATE_USER_SCHEMA: HaFormSchema[] = [
   {
     name: "username",
     required: true,
-    selector: { text: { autocomplete: "username" } },
+    selector: { text: { type: "email", autocomplete: "username" } },
   },
   {
     name: "password",
@@ -38,6 +39,16 @@ const CREATE_USER_SCHEMA: HaFormSchema[] = [
     name: "password_confirm",
     required: true,
     selector: { text: { type: "password", autocomplete: "new-password" } },
+  },
+  {
+    name: "secret_key",
+    required: true,
+    selector: { text: { autocomplete: "off" } },
+  },
+  {
+    name: "home_name",
+    required: true,
+    selector: { text: { autocomplete: "off" } },
   },
 ];
 
@@ -61,7 +72,9 @@ class OnboardingCreateUser extends LitElement {
     return html`
       <h1>${this.localize("ui.panel.page-onboarding.user.header")}</h1>
       <p>${this.localize("ui.panel.page-onboarding.user.intro")}</p>
-
+      ${this._loading
+        ? html`<onboarding-loading margin="5px auto"></onboarding-loading>`
+        : ""}
       ${this._errorMsg
         ? html`<ha-alert alert-type="error">${this._errorMsg}</ha-alert>`
         : ""}
@@ -84,6 +97,8 @@ class OnboardingCreateUser extends LitElement {
           !this._newUser.username ||
           !this._newUser.password ||
           !this._newUser.password_confirm ||
+          !this._newUser.secret_key ||
+          !this._newUser.home_name ||
           this._newUser.password !== this._newUser.password_confirm}
         >
           ${this.localize("ui.panel.page-onboarding.user.create_account")}
@@ -122,15 +137,17 @@ class OnboardingCreateUser extends LitElement {
   private _handleValueChanged(
     ev: ValueChangedEvent<HaFormDataContainer>
   ): void {
-    const nameChanged = ev.detail.value.name !== this._newUser.name;
-    const usernameChanged = ev.detail.value.username !== this._newUser.username;
+    // const nameChanged = ev.detail.value.name !== this._newUser.name;
     const passwordChanged =
       ev.detail.value.password !== this._newUser.password ||
       ev.detail.value.password_confirm !== this._newUser.password_confirm;
     this._newUser = ev.detail.value;
+    // disabled auto email-completing
+    /* 
     if (nameChanged) {
       this._maybePopulateUsername();
     }
+    */
     if (passwordChanged) {
       if (this._formError.password_confirm) {
         this._checkPasswordMatch();
@@ -138,9 +155,9 @@ class OnboardingCreateUser extends LitElement {
         this._debouncedCheckPasswordMatch();
       }
     }
-    if (usernameChanged) {
-      this._checkUsername();
-    }
+    // if (usernameChanged) {
+    //   this._checkUsername();
+    // }
   }
 
   private _debouncedCheckPasswordMatch = debounce(
@@ -162,6 +179,8 @@ class OnboardingCreateUser extends LitElement {
     }
   }
 
+  // disabled auto email-completing
+  /*
   private _maybePopulateUsername(): void {
     if (!this._newUser.name || this._newUser.name === this._newUser.username) {
       return;
@@ -187,9 +206,11 @@ class OnboardingCreateUser extends LitElement {
       this.requestUpdate("_formError");
     }
   }
+  */
 
   private async _submitForm(ev): Promise<void> {
     ev.preventDefault();
+
     this._loading = true;
     this._errorMsg = "";
 
@@ -201,6 +222,8 @@ class OnboardingCreateUser extends LitElement {
         name: String(this._newUser.name),
         username: String(this._newUser.username),
         password: String(this._newUser.password),
+        secret_key: String(this._newUser.secret_key),
+        home_name: String(this._newUser.home_name),
         language: this.language,
       });
 
